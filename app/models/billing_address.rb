bb48fb2e6a0c8a1d8c44d2700d1543c31c6450ec
Matis121 <mateusz6246@gmail.com>
2026-03-17 21:10:55 +0100

    feat: add country selection for billing and shipping addresses with dropdowns and country name display

--- app/models/billing_address.rb
+++ app/models/billing_address.rb
@@ -1,6 +1,8 @@
 class BillingAddress < ApplicationRecord
   belongs_to :order
 
+  COUNTRIES = ShippingAddress::COUNTRIES
+
   # Walidacje wymagane tylko gdy zamówienie nie jest szkicem I potrzebna jest faktura
   validates :company_name, presence: true, if: -> { needs_invoice? && !order&.draft_status? }
   validates :first_name, presence: true, if: -> { needs_invoice? && !order&.draft_status? }
@@ -9,6 +11,10 @@ class BillingAddress < ApplicationRecord
   validates :city, presence: true, if: -> { needs_invoice? && !order&.draft_status? }
   validates :postal_code, presence: true, if: -> { needs_invoice? && !order&.draft_status? }
   validates :country, presence: true, if: -> { needs_invoice? && !order&.draft_status? }
+
+  def country_name
+    COUNTRIES.find { |_name, code| code == country }&.first || country
+  end
 
   def self.ransackable_attributes(auth_object = nil)
     %w[company_name nip first_name last_name address_line1 city]
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,6 +1,18 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
 
+  COUNTRIES = [
+    [ "Polska", "PL" ], [ "Austria", "AT" ], [ "Belgia", "BE" ], [ "Bułgaria", "BG" ],
+    [ "Chorwacja", "HR" ], [ "Cypr", "CY" ], [ "Czechy", "CZ" ], [ "Dania", "DK" ],
+    [ "Estonia", "EE" ], [ "Finlandia", "FI" ], [ "Francja", "FR" ], [ "Grecja", "GR" ],
+    [ "Hiszpania", "ES" ], [ "Holandia", "NL" ], [ "Irlandia", "IE" ], [ "Litwa", "LT" ],
+    [ "Luksemburg", "LU" ], [ "Łotwa", "LV" ], [ "Malta", "MT" ], [ "Niemcy", "DE" ],
+    [ "Portugalia", "PT" ], [ "Rumunia", "RO" ], [ "Słowacja", "SK" ], [ "Słowenia", "SI" ],
+    [ "Szwecja", "SE" ], [ "Węgry", "HU" ], [ "Włochy", "IT" ],
+    [ "Norwegia", "NO" ], [ "Szwajcaria", "CH" ], [ "Ukraina", "UA" ],
+    [ "Wielka Brytania", "GB" ], [ "Stany Zjednoczone", "US" ]
+  ].freeze
+
   # Walidacje wymagane tylko gdy zamówienie nie jest szkicem
   validates :first_name, presence: true, unless: -> { order&.draft_status? }
   validates :last_name, presence: true, unless: -> { order&.draft_status? }
@@ -9,7 +21,11 @@ class ShippingAddress < ApplicationRecord
   validates :postal_code, presence: true, unless: -> { order&.draft_status? }
   validates :country, presence: true, unless: -> { order&.draft_status? }
 
+  def country_name
+    COUNTRIES.find { |_name, code| code == country }&.first || country
+  end
+
   def self.ransackable_attributes(auth_object = nil)
-    %w[first_name last_name address_line1 city postal_code]
+    %w[first_name last_name address_line1 city postal_code country]
   end
 end
